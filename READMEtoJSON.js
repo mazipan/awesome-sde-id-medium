4d5f5a281dc01d78ef890aee2c713e9dc5d32ca7
Irfan Maulana <mazipanneh@gmail.com>
2018-10-22 12:04:23 +0700

    Update READMEtoJSON.js

--- READMEtoJSON.js
+++ READMEtoJSON.js
@@ -2,16 +2,18 @@ const fs = require('fs')
 
 const readme = fs.readFileSync('README.md').toString()
 const regex = /^\+\s(.+):\s\[@(.+)\]\((http.+)\)/
+const jsonFilename = 'data-generated.json'
 
-var match, data = []
+let match, data = []
 for (var line of readme.split('\n')) {
-	match = line.match(regex)
-	if (match) {
-		data.push({
-			fullname: `${match[1]}`,
-			username: `${match[2]}`,
-			link: `${match[3]}`
-		})
-	}
+  match = line.match(regex)
+  if (match) {
+    data.push({
+      fullname: `${match[1]}`,
+      username: `${match[2]}`,
+      link: `${match[3]}`
+    })
+  }
 }
-fs.writeFileSync('List.json', JSON.stringify(data, false, 2))
+
+fs.writeFileSync(jsonFilename, JSON.stringify(data, false, 2))
